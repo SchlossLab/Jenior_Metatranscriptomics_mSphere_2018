99a7ba071d34a3f3df0420288c417614726e29ce
Matt Jenior <mljenior@umich.edu>
2017-05-10 21:23:05 -0400

    rf code

--- code/R/metabolome_rf.R
+++ code/R/metabolome_rf.R
@@ -7,7 +7,8 @@ starting_dir <- getwd()
 source('~/Desktop/Repositories/Jenior_Metatranscriptomics_2016/code/R/functions.R')
 
 # Output plot name
-plot_file <- 'results/supplement/figures/figure_S4.pdf'
+plot_file1 <- 'results/supplement/figures/figure_S5.pdf'
+plot_file2 <- 'results/supplement/figures/figure_S6.pdf'
 
 # Input Metabolomes
 metabolome_file <- 'data/metabolome/metabolomics.tsv'
@@ -74,7 +75,7 @@ metabolome_infection <- subset(metabolome_metadata, abx != 'none')
 metabolome_infection$abx <- NULL
 metabolome_infection$susceptibility <- NULL
 metabolome_infection$infection <- factor(metabolome_infection$infection)
-rm(metadata, metabolome_metadata)
+rm(metadata, metabolome, metabolome_metadata)
 
 #--------------------------------------------------------------------#
 
@@ -102,37 +103,23 @@ susceptibility_importances$label <- rownames(susceptibility_importances)
 susceptibility_accuracy <- paste('Accuracy = ',as.character((100-round((median(susceptibility_rf$err.rate[,1])*100), 2))),'%',sep='')
 rm(susceptibility_rf)
 
-# Subset to the most important OTUs and sort
+# Subset to the most important OTUs and sort, subset to top 10
 susceptibility_importances <- subset(susceptibility_importances, susceptibility_importances$MeanDecreaseAccuracy > abs(min(susceptibility_importances$MeanDecreaseAccuracy)))
 susceptibility_importances <- as.data.frame(susceptibility_importances[order(-susceptibility_importances$MeanDecreaseAccuracy),])
-susceptibility_importances <- susceptibility_importances[1:50,]
-susceptibility_importances <- as.data.frame(susceptibility_importances[order(susceptibility_importances$MeanDecreaseAccuracy),])
+susceptibility_importances <- susceptibility_importances[1:10,]
 
-# Reassociate metabolite names
+# Reassociate metabolite names - importances
 susceptibility_importances <- merge(metabolites, susceptibility_importances, by='label')
+susceptibility_importances$metabolites <- gsub('_', ' ', susceptibility_importances$metabolites)
 rownames(susceptibility_importances) <- susceptibility_importances$metabolites
 susceptibility_importances$metabolites <- NULL
 
-# Subset important metabolite relative concentrations, replace metabolite names
-metabolome_susceptibility <- metabolome_susceptibility[, c(1,which(colnames(metabolome_susceptibility) %in% susceptibility_importances$label))]
-temp_vector <- metabolome_susceptibility$susceptibility
-metabolome_susceptibility$susceptibility <- NULL
-metabolome_susceptibility <- t(metabolome_susceptibility)
-metabolome_susceptibility <- merge(metabolites, metabolome_susceptibility, by.x='label', by.y='row.names', all.y=TRUE)
-rownames(metabolome_susceptibility) <- metabolome_susceptibility$metabolites
-metabolome_susceptibility$metabolites <- NULL
-metabolome_susceptibility$label <- NULL
-metabolome_susceptibility <- as.data.frame(t(metabolome_susceptibility))
-metabolome_susceptibility$susceptibility <- temp_vector
-rm(temp_vector)
-
 # Break into experimental groups
+metabolome_susceptibility <- metabolome_susceptibility[, c(1,which(colnames(metabolome_susceptibility) %in% susceptibility_importances$label))]
 metabolome_susceptible <- subset(metabolome_susceptibility, susceptibility == 'susceptible')
 metabolome_susceptible$susceptibility <- NULL
-metabolome_susceptible <- metabolome_susceptible[,match(rownames(susceptibility_importances), colnames(metabolome_susceptible))] 
 metabolome_resistant <- subset(metabolome_susceptibility, susceptibility == 'resistant')
 metabolome_resistant$susceptibility <- NULL
-metabolome_resistant <- metabolome_resistant[,match(rownames(susceptibility_importances), colnames(metabolome_resistant))]
 rm(metabolome_susceptibility)
 
 #------------------#
@@ -164,38 +151,21 @@ rm(infection_rf)
 # Subset to the most important OTUs and sort
 infection_importances <- subset(infection_importances, infection_importances$MeanDecreaseAccuracy > abs(min(infection_importances$MeanDecreaseAccuracy)))
 infection_importances <- as.data.frame(infection_importances[order(-infection_importances$MeanDecreaseAccuracy),])
-infection_importances <- infection_importances[1:50,]
-infection_importances <- as.data.frame(infection_importances[order(infection_importances$MeanDecreaseAccuracy),])
+infection_importances <- infection_importances[1:10,]
 
-# Reassociate metabolite names
+# Reassociate metabolite names - importances
 infection_importances <- merge(metabolites, infection_importances, by='label')
+infection_importances$metabolites <- gsub('_', ' ', infection_importances$metabolites)
 rownames(infection_importances) <- infection_importances$metabolites
 infection_importances$metabolites <- NULL
 
-# Subset important metabolite relative concentrations
-metabolome_infection <- metabolome_infection[, c(1,which(colnames(metabolome_infection) %in% infection_importances$label))]
-
-# Subset important metabolite relative concentrations, replace metabolite names
-metabolome_infection <- metabolome_infection[, c(1,which(colnames(metabolome_infection) %in% infection_importances$label))]
-temp_vector <- metabolome_infection$infection
-metabolome_infection$infection <- NULL
-metabolome_infection <- t(metabolome_infection)
-metabolome_infection <- merge(metabolites, metabolome_infection, by.x='label', by.y='row.names', all.y=TRUE)
-rownames(metabolome_infection) <- metabolome_infection$metabolites
-metabolome_infection$metabolites <- NULL
-metabolome_infection$label <- NULL
-metabolome_infection <- as.data.frame(t(metabolome_infection))
-metabolome_infection$infection <- temp_vector
-rm(temp_vector)
-
 # Break into experimental groups
+metabolome_infection <- metabolome_infection[, c(1,which(colnames(metabolome_infection) %in% infection_importances$label))]
 metabolome_infected <- subset(metabolome_infection, infection == '630')
 metabolome_infected$infection <- NULL
-metabolome_infected <- metabolome_infected[,match(rownames(infection_importances), colnames(metabolome_infected))] 
 metabolome_mock <- subset(metabolome_infection, infection == 'mock')
 metabolome_mock$infection <- NULL
-metabolome_mock <- metabolome_mock[,match(rownames(infection_importances), colnames(metabolome_mock))]
-rm(metabolome, metabolome_infection)
+rm(metabolome_infection)
 
 #--------------------------------------------------------------------#
 
@@ -254,70 +224,116 @@ rm(infection_pvalues)
 
 #--------------------------------------------------------------------#
 
-# Subset everything to top 15 to make plotting easier
-# susceptibility
-metabolome_susceptible <- metabolome_susceptible[,1:15]
-metabolome_resistant <- metabolome_resistant[,1:15]
-susceptibility_importances <- susceptibility_importances[1:15,]
-# infection
-metabolome_infected <- metabolome_infected[,1:15]
-metabolome_mock <- metabolome_mock[,1:15]
-infection_importances <- infection_importances[1:15,]
+# Sort groups according to decreasing importance
+susceptibility_importances <- as.data.frame(susceptibility_importances[order(susceptibility_importances$MeanDecreaseAccuracy),])
+metabolome_susceptible <- metabolome_susceptible[susceptibility_importances$label]
+metabolome_resistant <- metabolome_resistant[susceptibility_importances$label]
+infection_importances <- as.data.frame(infection_importances[order(infection_importances$MeanDecreaseAccuracy),])
+metabolome_infection <- metabolome_infection[infection_importances$label]
+metabolome_mock <- metabolome_mock[infection_importances$label]
+
+# Reassociate metabolite names - concentrations
+metabolome_susceptible <- t(metabolome_susceptible)
+metabolome_susceptible <- merge(metabolome_susceptible, metabolites, by.y='label', by.x='row.names', all.x=TRUE)
+metabolome_susceptible$metabolites <- gsub('_', ' ', metabolome_susceptible$metabolites)
+rownames(metabolome_susceptible) <- metabolome_susceptible$metabolites
+metabolome_susceptible$metabolites <- NULL
+metabolome_susceptible$label <- NULL
+metabolome_susceptible$Row.names <- NULL
+metabolome_susceptible <- as.data.frame(t(metabolome_susceptible))
+metabolome_resistant <- t(metabolome_resistant)
+metabolome_resistant <- merge(metabolome_resistant, metabolites, by.y='label', by.x='row.names', all.x=TRUE)
+metabolome_resistant$metabolites <- gsub('_', ' ', metabolome_resistant$metabolites)
+rownames(metabolome_resistant) <- metabolome_resistant$metabolites
+metabolome_resistant$metabolites <- NULL
+metabolome_resistant$label <- NULL
+metabolome_resistant$Row.names <- NULL
+metabolome_resistant <- as.data.frame(t(metabolome_resistant))
+metabolome_infected <- t(metabolome_infected)
+metabolome_infected <- merge(metabolome_infected, metabolites, by.y='label', by.x='row.names', all.x=TRUE)
+metabolome_infected$metabolites <- gsub('_', ' ', metabolome_infected$metabolites)
+rownames(metabolome_infected) <- metabolome_infected$metabolites
+metabolome_infected$metabolites <- NULL
+metabolome_infected$label <- NULL
+metabolome_infected$Row.names <- NULL
+metabolome_infected <- as.data.frame(t(metabolome_infected))
+metabolome_mock <- t(metabolome_mock)
+metabolome_mock <- merge(metabolome_mock, metabolites, by.y='label', by.x='row.names', all.x=TRUE)
+metabolome_mock$metabolites <- gsub('_', ' ', metabolome_mock$metabolites)
+rownames(metabolome_mock) <- metabolome_mock$metabolites
+metabolome_mock$metabolites <- NULL
+metabolome_mock$label <- NULL
+metabolome_mock$Row.names <- NULL
+metabolome_mock <- as.data.frame(t(metabolome_mock))
+rm(metabolites)
 
 #--------------------------------------------------------------------#
 
 # Set up plotting environment
-pdf(file=plot_file, width=11, height=6)
+pdf(file=plot_file1, width=11, height=6)
 layout(matrix(c(1,2,2), nrow=1, ncol=3, byrow=TRUE))
 
 #---------------------#
 
 # Susceptibility
 # RF mean decrease accuracy
-par(mar=c(1.8,3,1,1), xaxs='i', xaxt='n', xpd=FALSE, mgp=c(2,0.2,0))
-dotchart(susceptibility_importances$MeanDecreaseAccuracy, labels=rownames(susceptibility_importances),
+#par(mar=c(1.8,3,1,1), xaxs='i', xaxt='n', xpd=FALSE, mgp=c(2,0.2,0))
+#dotchart(susceptibility_importances$MeanDecreaseAccuracy, labels=rownames(susceptibility_importances),
+#         lcolor=NA, cex=1.2, color='black', 
+#         xlab='', xlim=c(3,5), pch=19, lwd=3)
+#segments(x0=rep(3, 15), y0=c(1:10), x1=rep(5, 15), y1=c(1:10), lty=2) # Dotted lines
+#legend('bottomright', legend=susceptibility_accuracy, pt.cex=0, cex=1.2, bty='n')
+#par(xaxt='s')
+#axis(side=1, at=c(3:5), labels=c(0,4,5), cex.axis=1.2, tck=-0.025)
+#axis.break(1, 3.2, style='slash')
+#mtext('Mean Decrease Accuracy', side=1, padj=1.8, cex=0.9)
+#mtext('A', side=2, line=2, las=2, adj=1, padj=-13.2, cex=1.7)
+
+
+
+pdf(file='results/supplement/figures/figure_rf.pdf', width=9, height=5)
+par(mar=c(1.8,3,1,1), xaxs='i', xaxt='n', xpd=FALSE, mgp=c(2.5,0.5,0))
+dotchart(infection_importances$MeanDecreaseAccuracy, labels=rownames(infection_importances),
          lcolor=NA, cex=1.2, color='black', 
-         xlab='', xlim=c(8,15), pch=19, lwd=3)
-segments(x0=rep(8, 10), y0=c(1:15), x1=rep(15, 10), y1=c(1:15), lty=2) # Dotted lines
-legend('bottomright', legend=susceptibility_accuracy, pt.cex=0, cex=1.2, bty='n')
+         xlab='', xlim=c(7,14), pch=19, lwd=3)
+segments(x0=rep(7, 10), y0=c(1:10), x1=rep(14, 10), y1=c(1:10), lty=2)
+legend('bottomright', legend=infection_accuracy, pt.cex=0, cex=1.2, bty='n')
 par(xaxt='s')
-axis(side=1, at=c(8:15), labels=c(0,9:15), cex.axis=1.2, tck=-0.025)
-axis.break(1, 8.5, style='slash')
+axis(side=1, at=c(7:14), labels=c(0,8:14), cex.axis=1.2, tck=-0.05)
+axis.break(1, 7.5, style='slash')
 mtext('Mean Decrease Accuracy', side=1, padj=1.8, cex=0.9)
-mtext('A', side=2, line=2, las=2, adj=1, padj=-13.2, cex=1.7)
+dev.off()
+
+
+
 
 # OTU abundance differences
-par(mar=c(3,19,1,1), xaxs='r', mgp=c(2,1,0))
-plot(1, type='n', ylim=c(0.8, (ncol(cleared_preabx_shared)*2)-0.8), xlim=c(0,3), 
-     ylab='', xlab='Abundance', xaxt='n', yaxt='n', cex.lab=1.4)
-index <- 1
-for(i in colnames(cleared_preabx_shared)){
-  stripchart(at=index+0.35, cleared_preabx_shared[,i], 
-             pch=21, bg='firebrick1', method='jitter', jitter=0.15, cex=1.7, lwd=0.5, add=TRUE)
-  stripchart(at=index-0.35, colonized_preabx_shared[,i], 
-             pch=21, bg='dodgerblue1', method='jitter', jitter=0.15, cex=1.7, lwd=0.5, add=TRUE)
-  if (i != colnames(cleared_preabx_shared)[length(colnames(cleared_preabx_shared))]){
-    abline(h=index+1, lty=2)
-  }
-  segments(median(cleared_preabx_shared[,i]), index+0.6, median(cleared_preabx_shared[,i]), index+0.1, lwd=2.5) #adds line for median
-  segments(median(colonized_preabx_shared[,i]), index-0.6, median(colonized_preabx_shared[,i]), index-0.1, lwd=2.5)
-  index <- index + 2
-}
-axis(side=1, at=c(0:3), label=c('0','10','100','1000'), cex.axis=1.2, tck=-0.02)
-minors <- c(0.1,0.28,0.44,0.58,0.7,0.8,0.88,0.94,0.98)
-axis(side=1, at=minors, label=rep('',length(minors)), tck=-0.01)
-axis(side=1, at=minors+1, label=rep('',length(minors)), tck=-0.01)
-axis(side=1, at=minors+2, label=rep('',length(minors)), tck=-0.01)
-legend('topright', legend=c('Cleared', 'Colonized'),
-       pch=c(21, 21), pt.bg=c('firebrick1','dodgerblue1'), bg='white', pt.cex=1.7, cex=1.2)
-axis(2, at=seq(1,index-2,2)+0.6, labels=rownames(preabx_importances), las=1, line=-0.5, tick=F, cex.axis=1.4)
-formatted_taxa <- lapply(1:nrow(preabx_importances), function(x) bquote(paste(.(preabx_importances$phylum[x]),'; ',italic(.(preabx_importances$genus[x])), sep='')))
-axis(2, at=seq(1,index-2,2), labels=do.call(expression, formatted_taxa), las=1, line=-0.5, tick=F, cex.axis=1.1, font=3) 
-italic_p <- lapply(1:length(preabx_importances$pvalues), function(x) bquote(paste(italic('p'), .(preabx_importances$pvalues[x]), sep=' ')))
-axis(2, at=seq(1,index-2,2)-0.6, labels=do.call(expression, italic_p), las=1, line=-0.5, tick=F, cex.axis=1.2, font=3) 
-mtext('B', side=2, line=2, las=2, adj=13, padj=-13, cex=1.7)
+#par(mar=c(3,19,1,1), xaxs='r', mgp=c(2,1,0))
+#plot(1, type='n', ylim=c(0.8, (ncol(metabolome_susceptible)*2)-0.8), xlim=c(0,3), 
+#     ylab='', xlab='Abundance', xaxt='n', yaxt='n', cex.lab=1.4)
+#index <- 1
+#for(i in colnames(metabolome_susceptible)){
+#  stripchart(at=index+0.35, metabolome_susceptible[,i], 
+#             pch=21, bg='firebrick1', method='jitter', jitter=0.15, cex=1.7, lwd=0.5, add=TRUE)
+#  stripchart(at=index-0.35, colonized_preabx_shared[,i], 
+#             pch=21, bg='dodgerblue1', method='jitter', jitter=0.15, cex=1.7, lwd=0.5, add=TRUE)
+#  if (i != colnames(metabolome_susceptible)[length(colnames(metabolome_susceptible))]){
+#    abline(h=index+1, lty=2)
+#  }
+#  segments(median(metabolome_susceptible[,i]), index+0.6, median(metabolome_susceptible[,i]), index+0.1, lwd=2.5) #adds line for median
+#  segments(median(colonized_preabx_shared[,i]), index-0.6, median(colonized_preabx_shared[,i]), index-0.1, lwd=2.5)
+#  index <- index + 2
+#}
+#axis(side=1, at=c(0:3), label=c('0','10','100','1000'), cex.axis=1.2, tck=-0.02)
+#minors <- c(0.1,0.28,0.44,0.58,0.7,0.8,0.88,0.94,0.98)
+#axis(side=1, at=minors, label=rep('',length(minors)), tck=-0.01)
+#axis(side=1, at=minors+1, label=rep('',length(minors)), tck=-0.01)
+#axis(side=1, at=minors+2, label=rep('',length(minors)), tck=-0.01)
+#legend('topright', legend=c('Cleared', 'Colonized'),
+#       pch=c(21, 21), pt.bg=c('firebrick1','dodgerblue1'), bg='white', pt.cex=1.7, cex=1.2)
+#axis(2, at=seq(1,index-2,2)+0.6, labels=colnames(metabolome_susceptible), las=1, line=-0.5, tick=F, cex.axis=1.4)
 
-dev.off()
+#dev.off()
 
 #-------------------------------------------------------------------------------------------------------------------------------------#
 
