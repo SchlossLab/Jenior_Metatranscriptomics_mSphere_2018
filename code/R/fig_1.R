24747a85e9632acb051472e4fe7f8e577c2b7bf1
Matt Jenior <mljenior@umich.edu>
2017-07-12 09:13:01 -0400

    removing error for fig 1

--- code/R/fig_1.R
+++ code/R/fig_1.R
@@ -19,7 +19,10 @@ plot_file <- 'results/figures/figure_1.pdf'
 
 # Load in data
 metadata <- read.delim(metadata_file, sep='\t', header=T, row.names=1)
+<<<<<<< Updated upstream
 metadata <- subset(metadata, type == 'conventional') # remove germfree
+=======
+>>>>>>> Stashed changes
 metadata$type <- NULL
 metadata$cage <- NULL
 metadata$mouse <- NULL
