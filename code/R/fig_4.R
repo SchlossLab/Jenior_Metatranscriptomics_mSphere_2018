f0edfd89295d8d71481298b0182216534ba60881
Matt Jenior <mljenior@umich.edu>
2017-09-10 20:21:15 -0400

    adding pat edits

--- code/R/fig_4.R
+++ code/R/fig_4.R
@@ -50,6 +50,8 @@ metabolome$SUB_PATHWAY <- NULL
 metabolome$SUPER_PATHWAY <- NULL
 metabolome <- as.data.frame(t(metabolome))
 
+aminovalerate <- subset(aminovalerate, abx != 'germfree')
+
 #-------------------------------------------------------------------------------------------------------------------------#
 
 # Ordination
@@ -251,50 +253,43 @@ metabolite_stripchart(plot_f, inf_clinda_metabolome, mock_clinda_metabolome, cli
                       44.44, 'Infected', 'Mock', 'Clindamycin-pretreated', clinda_col, 'F')
 
 # Aminovalerate
-pdf(file=plot_g, width=8, height=4)
+pdf(file=plot_g, width=6, height=4)
 par(mar=c(3.5,5,1.5,1), xpd=FALSE, las=1, mgp=c(3,0.7,0))
-
 stripchart(substrate~infection, data=aminovalerate_untreated, vertical=T, pch=19, 
-           xaxt='n', yaxt='n', col='gray40', ylim=c(0,6), xlim=c(0.5,13.5),
+           xaxt='n', yaxt='n', col='gray40', ylim=c(0,6), xlim=c(0.5,10.5),
            cex=1.5, ylab='Scaled Intesity', method='jitter', jitter=0.15, cex.lab=1.2)
 stripchart(substrate~infection, data=aminovalerate_strep, vertical=T, pch=19, at=c(3,4),
-           xaxt='n', yaxt='n', col=strep_col, ylim=c(0,6), xlim=c(0.5,13.5),
+           xaxt='n', yaxt='n', col=strep_col, ylim=c(0,6), xlim=c(0.5,10.5),
            cex=1.5, ylab='Scaled Intensity', method='jitter', jitter=0.15, cex.lab=1.2, add=TRUE)
 stripchart(substrate~infection, data=aminovalerate_cef, vertical=T, pch=19, at=c(6,7),
-           xaxt='n', yaxt='n', col=cef_col, ylim=c(0,6), xlim=c(0.5,13.5),
+           xaxt='n', yaxt='n', col=cef_col, ylim=c(0,6), xlim=c(0.5,10.5),
            cex=1.5, ylab='Scaled Intensity', method='jitter', jitter=0.15, cex.lab=1.2, add=TRUE)
 stripchart(substrate~infection, data=aminovalerate_clinda, vertical=T, pch=19, at=c(9,10),
-           xaxt='n', yaxt='n', col=clinda_col, ylim=c(0,6), xlim=c(0.5,13.5),
-           cex=1.5, ylab='Scaled Intensity', method='jitter', jitter=0.15, cex.lab=1.2, add=TRUE)
-stripchart(substrate~infection, data=aminovalerate_gf, vertical=T, pch=19, at=c(12,13),
-           xaxt='n', yaxt='n', col='forestgreen', ylim=c(0,6), xlim=c(0.5,13.5),
+           xaxt='n', yaxt='n', col=clinda_col, ylim=c(0,6), xlim=c(0.5,10.5),
            cex=1.5, ylab='Scaled Intensity', method='jitter', jitter=0.15, cex.lab=1.2, add=TRUE)
 axis(side=2, at=c(0:6), labels=c('0.0','1.0','2.0','3.0', '4.0','5.0','6.0'), cex.axis=1.2)
 abline(v=c(2,5,8,11), lty=2, col='gray35')
 mtext(c('CDI:','Group:'), side=1, at=-0.7, padj=c(0.3,2.5), cex=0.7)
-mtext(c('-','-','+','-','+','-','+','-','+'), side=1, 
-      at=c(1,3,4,6,7,9,10,12,13), padj=0.3, cex=1.1)
-mtext(c('No Antibiotics','Streptomycin','Cefoperazone','Clindamycin','ex-Germfree'), side=1, 
-      at=c(1,3.5,6.5,9.5,12.5), padj=2, cex=0.9)
-legend('topright', legend='5-Aminovalerate', pt.cex=0, bty='n')
-segments(x0=c(0.6,2.6,3.6,5.6,6.6,8.6,9.6,11.6,12.6), x1=c(1.4,3.4,4.4,6.4,7.4,9.4,10.4,12.4,13.4),
+mtext(c('-','-','+','-','+','-','+'), side=1, 
+      at=c(1,3,4,6,7,9,10), padj=0.3, cex=1.1)
+mtext(c('No Antibiotics','Streptomycin','Cefoperazone','Clindamycin'), side=1, 
+      at=c(1,3.5,6.5,9.5), padj=2, cex=0.9)
+legend('topright', legend='5-Aminovalerate', pt.cex=0, bty='n', cex=0.9)
+segments(x0=c(0.6,2.6,3.6,5.6,6.6,8.6,9.6), x1=c(1.4,3.4,4.4,6.4,7.4,9.4,10.4),
          y0=c(median(aminovalerate_untreated[,2]),
               median(subset(aminovalerate_strep, infection=='mock')[,2]), median(subset(aminovalerate_strep, infection=='infected')[,2]),
               median(subset(aminovalerate_cef, infection=='mock')[,2]), median(subset(aminovalerate_cef, infection=='infected')[,2]),
-              median(subset(aminovalerate_clinda, infection=='mock')[,2]), median(subset(aminovalerate_clinda, infection=='infected')[,2]),
-              median(subset(aminovalerate_gf, infection=='mock')[,2]), median(subset(aminovalerate_gf, infection=='infected')[,2])), 
+              median(subset(aminovalerate_clinda, infection=='mock')[,2]), median(subset(aminovalerate_clinda, infection=='infected')[,2])),
          y1=c(median(aminovalerate_untreated[,2]),
               median(subset(aminovalerate_strep, infection=='mock')[,2]), median(subset(aminovalerate_strep, infection=='infected')[,2]),
               median(subset(aminovalerate_cef, infection=='mock')[,2]), median(subset(aminovalerate_cef, infection=='infected')[,2]),
-              median(subset(aminovalerate_clinda, infection=='mock')[,2]), median(subset(aminovalerate_clinda, infection=='infected')[,2]),
-              median(subset(aminovalerate_gf, infection=='mock')[,2]), median(subset(aminovalerate_gf, infection=='infected')[,2])),
+              median(subset(aminovalerate_clinda, infection=='mock')[,2]), median(subset(aminovalerate_clinda, infection=='infected')[,2])),
          lwd=3)
-segments(x0=c(3,6,9,12), y0=5, x1=c(4,7,10,13), y1=5, lwd=2)
-text(x=c(3.5,6.5,9.5,12.5), y=5.2, '*', font=2, cex=2)
-mtext(rep('*',7), side=3, adj=c(0.21,0.28,
+segments(x0=c(3,6,9), y0=5, x1=c(4,7,10), y1=5, lwd=2)
+text(x=c(3.5,6.5,9.5), y=5.2, '*', font=2, cex=2)
+mtext(rep('*',5), side=3, adj=c(0.21,0.28,
                                 0.43,0.5,
-                                0.645,
-                                0.863,0.933), padj=0.4, font=2, cex=1.5, col='gray40') # Untreated vs Mock significance
+                                0.645), padj=0.4, font=2, cex=1.5, col='gray40') # Untreated vs Mock significance
 mtext('G', side=2, line=2, las=2, adj=2, padj=-8, cex=1.6, font=2)
 
 dev.off()
