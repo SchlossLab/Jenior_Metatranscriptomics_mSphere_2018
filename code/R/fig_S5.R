1fcd687db3694955663cb91b2b98647104f6f2d3
Matt Jenior <mljenior@umich.edu>
2018-02-06 12:53:18 -0500

    typo in figure

--- code/R/fig_S5.R
+++ code/R/fig_S5.R
@@ -312,7 +312,6 @@ strep$Row.names <- NULL
 clinda <- merge(clinda, bar_palette, by.x='row.names', by.y='pathway')
 rownames(clinda) <- clinda$Row.names
 clinda$Row.names <- NULL
-clinda$Metagenome <- clinda$Metagenome + 10 # Fix error
 noabx <- merge(noabx, bar_palette, by.x='row.names', by.y='pathway')
 rownames(noabx) <- noabx$Row.names
 noabx$Row.names <- NULL
